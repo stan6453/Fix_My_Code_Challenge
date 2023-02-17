b2846a780d9765516c93fb35a5291be89563648a
stanley <stanley6453@gmail.com>
2023-02-17 21:06:43 +0100

    modified /0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c

--- 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -43,14 +43,10 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	}
 	else
 	{
-		tmp = *head;
-		*head = (*head)->prev;
-		if (tmp-> next != NULL)
-		{
-			tmp->next->prev = *head;
-			(*head)->next = tmp->next;
-		}
-		free(tmp);
+		(*head)->prev->prev = (*head)->prev;
+		free(*head);
+		if ((*head)->next)
+			(*head)->next->prev = (*head)->prev;
 		*head = saved_head;
 	}
 	return (1);
